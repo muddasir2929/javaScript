bada7bed42f908f58db83ba454545643c0f4bfc3
muddasir2929 <muddasir.mm74@gmail.com>
2020-06-06 08:03:35 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+// alert("Hello world!");
+
+// alert("Error! Please enter a valid password.");
+
+// alert("How are you Muhammad?");
+
+// alert("What is your age?");
+
+
+// // for addition
+// var num1 = 3;
+// var num2 = 5;
+// var sum_of_num = num1 + num2;
+// alert(sum_of_num);
+
+// // for subtraction
+// var num1 = 3;
+// var num2 = 5;
+// var difference_of_num = num2 - num1;
+// alert(difference_of_num);
+
+// // for division
+// var num1 = 5;
+// var num2 = 3;
+// var division_of_num = num1 / num2;
+// alert(division_of_num);
+
+// for multiplication
+// var num1 = 5;
+// var num2 = 3;
+// var multiplication_of_num = num1 * num2;
+// alert(multiplication_of_num);
