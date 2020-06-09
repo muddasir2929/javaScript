fde721372185bd99b1810c31de0074d4e7588477
muddasir2929 <muddasir.mm74@gmail.com>
2020-06-09 11:32:56 +0500

    question # 4

--- chapter9-11.js
+++ chapter9-11.js
@@ -16,17 +16,36 @@
 // }
 
 // question # 3
-var signalColor;
-signalColor = prompt("Enter your signal color:");
-if (signalColor == "red") {
-    alert("Must stop!");
+// var signalColor;
+// signalColor = prompt("Enter your signal color:");
+// if (signalColor == "red") {
+//     alert("Must stop!");
+// }
+// else if (signalColor == "yellow") {
+//     alert("Ready to move!");
+// }
+// else if (signalColor == "green") {
+//     alert("Move now!");
+// }
+// else {
+//     alert("You are on the way!");
+// }
+
+// question # 4
+var remainingFuel;
+remainingFuel = prompt("How many liters of fuel are left?");
+if (remainingFuel < 0.25){
+    alert("Please refill the fuel in your car!");
 }
-else if (signalColor == "yellow") {
-    alert("Ready to move!");
+else if (remainingFuel >= 0.25){
+    alert("you may have fuel shortage!");
 }
-else if (signalColor == "green") {
-    alert("Move now!");
+else if (remainingFuel >= 2){
+        alert("You have enough fuel to drive!");
+}
+else if (remainingFuel == 4){
+    alert("You have surplus fuel in your car!");
 }
 else {
-    alert("You are on the way!");
+    alert("You must check feul befor drive!");
 }
