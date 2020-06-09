ea8187c62e8dcf3a30b17539249d653886025b1a
muddasir2929 <muddasir.mm74@gmail.com>
2020-06-08 22:53:19 -0700

    chapter 9-11 two question solved

---
+++ chapter9-11.js
@@ -0,0 +1,16 @@
+// question # 1
+// var city;
+// city = prompt("Enter your city name");
+// if(city == "karachi"){
+//     alert("welcom to the city of lights.")
+// }
+
+// question # 2
+var gender;
+gender = prompt("Please specify your gender");
+if (gender == "male"){
+    alert("Good Morning Sir!");
+}
+else{
+    alert("Good Morning Ma'am!");
+}
