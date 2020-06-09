609a0196e38c6ca14122c3e5f086649cb4472fad
muddasir2929 <muddasir.mm74@gmail.com>
2020-06-08 23:04:11 -0700

    question # 3

--- chapter9-11.js
+++ chapter9-11.js
@@ -6,11 +6,27 @@
 // }
 
 // question # 2
-var gender;
-gender = prompt("Please specify your gender");
-if (gender == "male"){
-    alert("Good Morning Sir!");
+// var gender;
+// gender = prompt("Please specify your gender");
+// if (gender == "male"){
+//     alert("Good Morning Sir!");
+// }
+// else{
+//     alert("Good Morning Ma'am!");
+// }
+
+// question # 3
+var signalColor;
+signalColor = prompt("Enter your signal color:");
+if (signalColor == "red") {
+    alert("Must stop!");
 }
-else{
-    alert("Good Morning Ma'am!");
+else if (signalColor == "yellow") {
+    alert("Ready to move!");
+}
+else if (signalColor == "green") {
+    alert("Move now!");
+}
+else {
+    alert("You are on the way!");
 }
