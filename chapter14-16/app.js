dff7526b0263e51e59a9934fd3165c8d5a452a6e
muddasir2929 <muddasir.mm74@gmail.com>
2020-06-10 22:06:45 +0500

    8 questions done

--- chapter14-16/app.js
+++ chapter14-16/app.js
@@ -1,12 +1,48 @@
 // question # 1 array literal notation
-var studentName;
-studentName = [];
+// var studentName;
+// studentName = [];
 
 // question # 1 alternative
-var array;
-array = ["red", "brown", "green", "orange"];
+// var array;
+// array = ["red", "brown", "green", "orange"];
 
 // qeustion # 2 object literal notation
-var person;
-person = {a : 1, b = "test"}
+// var person;
+// person = {a : 1, b = "test"}
 // object literal notation consits of key value pairs comma delimited and wrapped in curly brackets
+
+// question # 3 string array
+// var string = ["asad", "ahmed", "ali", "zaid"];
+
+// qestion # 4 number array
+// var number = [1, 2, 3, 4, 5];
+
+// question # 5 boolian array
+// var boolian = [true, false];
+
+// question # 6 mixed array
+// var mixed = ["asad", "ahmed", 3, 4, true, false];
+
+// question # 7
+//   var Qualification = ["SSC","HSC","BSC","BS","BCOM","MS","MPHIL","PHD"]
+//   var NewQualification = Qualification.map( name => name)
+//   document.write(NewQualification);
+
+// question # 8
+// // var Students = ["Abid", "Ali", "Arif"];
+// var score = [320, 230, 480];
+// var totalScore = 500;
+
+// question # 9
+
+// question # 10
+
+// question # 11
+
+// question # 12
+
+// question # 13
+
+// question # 14
+
+// question # 15
