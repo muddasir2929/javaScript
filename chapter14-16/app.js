1fe25eae63b40dfc30633abe75075d0c1f4fb581
muddasir2929 <muddasir.mm74@gmail.com>
2020-06-10 18:09:04 +0500

    object literal notation

---
+++ chapter14-16/app.js
@@ -0,0 +1,12 @@
+// question # 1 array literal notation
+var studentName;
+studentName = [];
+
+// question # 1 alternative
+var array;
+array = ["red", "brown", "green", "orange"];
+
+// qeustion # 2 object literal notation
+var person;
+person = {a : 1, b = "test"}
+// object literal notation consits of key value pairs comma delimited and wrapped in curly brackets
