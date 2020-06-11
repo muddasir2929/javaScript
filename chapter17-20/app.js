a34e55ad8193852dbd164c803842da0bf010240a
muddasir2929 <muddasir.mm74@gmail.com>
2020-06-11 10:34:07 +0500

    updated assignment

---
+++ chapter17-20/app.js
@@ -0,0 +1,39 @@
+question # 1
+alert("Hello User!")
+
+question # 2
+alert("Error! Please enter a valid password")
+
+question # 3
+alert("Welcome to Js land... \n Happy coding");
+
+question # 4
+alert("Welcom to JS Land");
+
+alert("Happy Coding! \n Prevent this page from creating additional dialoge.");
+
+console.log("Hello... I can run JS through my web browser's console.");
+
+var username = "Muhammad";
+var myName = "Muhammad" "Muddasir";
+alert(myName);
+
+var message;
+message = "Hello World!";
+alert(message);
+
+var myName;
+myName = "Muhammad" "Muddasir";
+alert(myName);
+
+var age;
+age = "24";
+alert(age);
+
+var profession;
+profession = "Certified Mobile Web Application Development";
+alert(profession);
+
+var foodItem;
+foodItem = "Pizza \n Pizz \n piz \n pi \n p";
+alert(foodItem);
---
+++ chapter6-9/app.js
@@ -0,0 +1,57 @@
+// // <!-- question # 1 -->
+// var a;
+// a = 10;
+// document.write("Result:" + '<br />' + "The value of a is:" + " " + " " + a);
+// ++a;
+// document.write('<br />' + '<br />' + "The value of" + " " + "++a" + " " + "is:" + " " + a);
+// document.write('<br />' + "Now the value of a is:" + " " + a);
+// document.write('<br />' + '<br />' + "The value of a++ is: " + " " + a);
+// a++;
+// document.write('<br />' + " " + "Now the value of a is: " + " " + a);
+// --a;
+// document.write('<br />' + '<br />' + "The value of --a is: " + " " + a);
+// document.write('<br />' + "Now the value of a is: " + " " + a);
+// document.write('<br />' + '<br />' + "The value of a-- is: " + " " + a);
+// a--;
+// document.write('<br />' + "Now the value of a is: " + " " + a);
+
+
+// question # 2
+//     var a = 2, b = 1;
+// var result = --a - --b + ++b + b--;
+// document.write('<br />' + '<br />' + " " + result);
+// --a; // a = 1 output is 1
+// --a - --b; // a = 1, b = 0 output is 1
+// --a - --b + ++b; // a = 1, b = 0, b = 1 output is 1 - 0 + 1 = 2
+// --a - --b + ++b + b--; // a = 1, b = 0, b = 1, b = 1 output is 1 - 0 + 1 + 1 = 3
+// document.write('<br />' + '<br />' + "a is: " + " " + "1");
+// document.write('<br />' + "b is: " + " " + "2");
+// document.write('<br />' + "result is: " + " " + "3");
+
+// question # 3
+// var name = prompt("What is your name?");{
+//     if (name == "Muhammad");
+//     alert("Hi" + " " + name + "!");
+// }
+
+// question # 4
+// var num = prompt("Enter your table name.");
+// for (var i = 1; i <= 10; i++) {
+//     console.log(num + " " + "X " + i + " = " + (num * i));
+// }
+
+// question # 6
+// var numOfCats = prompt("How many cats?");
+// var tooManyCats = numOfCats + 1;
+// alert(tooManyCats);
+
+
+// question # 7
+// var a = prompt("Enter your Subject name!");
+// var x = "English";
+// var y = "Math";
+// var z = "Urdu";
+// var totalMarks = 100;
+// if (a === "English", "Math", "Urdu") {
+//     alert("100 Marks");
+// }
