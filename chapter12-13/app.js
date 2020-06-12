356f2293dcd052dfde4c9a69f827173cae68d56b
muddasir2929 <muddasir.mm74@gmail.com>
2020-06-12 19:02:33 +0500

    updated Chapter 12-13

---
+++ chapter12-13/app.js
@@ -0,0 +1,58 @@
+// Question 1 
+// var greeting;
+// var hour = 13;
+// if (hour < 18) {
+// alert("Good Day")
+// }else{
+//  alert("Good Evening")
+// }
+
+
+// Question 2 
+// var time = prompt("Enter time");
+// if(time === "13"){
+//     alert("1")
+// }else if(time ==="14"){
+//     alert("2")
+// }else if(time === "15"){
+//     alert("3")
+// }else if(time === "16"){
+//     alert("4")
+// }else if(time === "17"){
+//     alert("5")
+// }else if(time === "18"){
+//     alert("6")
+// }else if(time === "19"){
+//     alert("7")
+// }else if(time === "20"){
+//     alert("8")
+// }else if(time === "21"){
+//     alert("9")
+// }else if(time === "22"){
+//     alert("10")
+// }else if(time === "23"){
+//     alert("11")
+// }else if(time === "24"){
+//     alert("12")
+// }else{
+//     alert("Sorry")
+// }
+
+
+// Question 3
+// var password = "samabii786"
+// var checkPassword = prompt("Enter your Password");
+// if(password == checkPassword){
+//     alert(" correct password")
+// }else{
+//     alert("Sorry Wrong password")
+// }
+
+// Question 4 
+// var vowelWords = ["a", "e", "i", "o", "u"];
+// var character = prompt("Enter character");
+// if (character === vowelWords[0]) {
+//     alert("True")
+// } else {
+//     alert("Wrong")
+// }
