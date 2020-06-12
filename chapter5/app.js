798ff7de1de0ad1461ab3955e4752af1749aefab
muddasir2929 <muddasir.mm74@gmail.com>
2020-06-12 18:52:59 +0500

    updated Chapter 5

---
+++ chapter5/app.js
@@ -0,0 +1,34 @@
+//  Question 1 
+// var x = 3;
+// var y = 5;
+// var z = x + y;
+// document.write("Sum of 3 and 5 is " + z);
+
+// Question 2
+// var z = x - y;
+// document.write("Subtraction of 3 and 5 is " + z);
+// var z = x * y;
+// document.write("Multipliaction of 3 and 5 is " + z);
+// var z = x / y;
+// document.write("Division of 3 and 5 is " + z);
+
+// Question 3
+//  var myNum;
+//  document.write("Value after variables declaration is " + myNum);
+//  var myNum = 5;
+//  document.write("INitial value is  " + myNum);
+//  myNum++;
+//  document.write(myNum);
+//   var newNum = myNum + 7;
+//  document.write("value after addition is " + newNum);
+//  --newNum;
+//  document.write("value after decreament is " + newNum);
+//  var reminder = newNum / 3;
+//  document.write('THe reminder is ' + reminder);
+
+
+// Question no 4
+// var oneMovieCost = 600;
+// var costOfFiveTickets;
+// var totalCost = 600 * 5;
+// document.write("Total cost to buy 5 tickets of a movie is " + totalCost + "PKR ");
