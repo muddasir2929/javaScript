331a613ff3df508bc80e4e13135983d03d657dea
muddasir2929 <muddasir.mm74@gmail.com>
2020-06-12 14:56:44 +0500

    updated Chapter 4

---
+++ chapter4/app.js
@@ -0,0 +1,34 @@
+// var x = 5;
+// var name = "chris";
+// var developer = true | false;
+// var blue = nul;
+// var blue =
+// alert(blue); 
+
+// // question # 1
+// var a = 10, b = 20, c = 30, x = 5, y = 6; z = 7;
+
+// or you can also assign variables as
+// var a = 3;
+// b = 5;
+// c = 7;
+// x = 10;
+// y = 20;
+// z = 30;
+// alert(c);
+
+
+// question # 2
+// legal variables
+// var myName123 = "Muhammad";
+// var _secondName12 = "Muddasir";
+// var $lastName = "fahad";
+// var $my_name = "ali ahmed";
+// var $my_second_123 = "ali";
+
+// illegal variables
+// var 12myName = "Muhammad";
+// var @secondName12 = "Muddasir";
+// var #lastName = "fahad";
+// var % my_name = "ali ahmed";
+// var & my_second_123 = "ali";
