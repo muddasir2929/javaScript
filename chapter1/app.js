9feb7165c94f19d16dd3246bf5c55f32e5ece779
muddasir2929 <muddasir.mm74@gmail.com>
2020-06-12 14:35:06 +0500

    updated Chapter 1

--- chapter1/app.js
+++ chapter1/app.js
@@ -1,1 +1,11 @@
 alert("Hello User");
+
+alert("Error! Please enter a valid password");
+
+alert("Welcome to Js land... \n Happy coding");
+
+alert("Welcom to JS Land"); 
+
+alert("Happy Coding! \n Prevent this page from creating additional dialoge.");
+
+console.log("Hello... I can run JS through my web browser's console.");
