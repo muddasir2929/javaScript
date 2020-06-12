27785e2ddfbb7f859026989651bd2768f1e32262
muddasir2929 <muddasir.mm74@gmail.com>
2020-06-12 14:45:14 +0500

    updated Chapter 2

---
+++ chapter2/app.js
@@ -0,0 +1,48 @@
+// question # 1
+// var username;
+
+// question # 2
+// var username = ""Muhammad"";
+// var myName = "Muhammad" "Muddasir";
+// alert(myName);
+
+// question # 3
+// var message;
+// message = "Hello World!";
+// alert(message);
+
+// question # 4
+// var myName;
+// myName = "Muhammad" "Muddasir";
+// alert(myName);
+
+// var age;
+// age = "24";
+// alert(age);
+
+// var profession;
+// profession = "Certified Mobile Web Application Development";
+// alert(profession);
+
+// question # 5
+// var foodItem;
+// foodItem = "Pizza \n Pizz \n piz \n pi \n p";
+// alert(foodItem);
+
+// question # 6
+// var email;
+// email = "muddasir.mm" + "29" + "@" + "gmail.com";
+// alert(email);
+
+// question # 7
+// var book;
+// book = "A Smarter way to learn JavaScript.";
+// alert(book);
+
+// question # 8
+// alert("Yeh! I can write HTML content through JavaScript.");
+
+// question # 9
+// var design;
+// design = "-------------------" + "&" + "%" + "%" + "&" + "-------------------";
+// alert(design);
