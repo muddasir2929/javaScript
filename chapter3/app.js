ba0bff1e69c4c9809be9939ae43738dc49525c14
muddasir2929 <muddasir.mm74@gmail.com>
2020-06-12 14:49:36 +0500

    updated Chapter 3

---
+++ chapter3/app.js
@@ -0,0 +1,22 @@
+// question # 1
+// var age;
+// age = "I am 15 years old.";
+// alert(age);
+
+// question # 2
+// var numberOfVisits;
+// numberOfVisits = "you have visited this site 14 times";
+// alert(numberOfVisits);
+
+// question # 3
+// var birthYear;
+// birthYear = "My birth year is 1997.";
+// alert(birthYear);
+
+// question # 4
+// var name = "John Doe" + " ";
+// var order = "ordered 5";
+// var product = " " + "T-shirts";
+// var site = " " + "on google.com";
+// var purchase = name + order + product + site;
+// alert(purchase);
