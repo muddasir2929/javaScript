525ac9cb206187cfff693aa6420b562430b8a37a
muddasir2929 <muddasir.mm74@gmail.com>
2020-06-13 20:45:00 +0500

    updated Chapter 17-20

--- chapter17-20/app.js
+++ chapter17-20/app.js
@@ -1,39 +1,7 @@
-question # 1
-alert("Hello User!")
-
-question # 2
-alert("Error! Please enter a valid password")
-
-question # 3
-alert("Welcome to Js land... \n Happy coding");
-
-question # 4
-alert("Welcom to JS Land");
-
-alert("Happy Coding! \n Prevent this page from creating additional dialoge.");
-
-console.log("Hello... I can run JS through my web browser's console.");
-
-var username = "Muhammad";
-var myName = "Muhammad" "Muddasir";
-alert(myName);
-
-var message;
-message = "Hello World!";
-alert(message);
-
-var myName;
-myName = "Muhammad" "Muddasir";
-alert(myName);
-
-var age;
-age = "24";
-alert(age);
-
-var profession;
-profession = "Certified Mobile Web Application Development";
-alert(profession);
-
-var foodItem;
-foodItem = "Pizza \n Pizz \n piz \n pi \n p";
-alert(foodItem);
+var animalsName;
+animalsName = ["dog", "cat", "mouse", "rabbit"];
+// animalsName.shift();
+// animalsName.unshift("horse");
+// animalsName.pop();
+animalsName.push("lion", "fox")
+document.write(animalsName);
