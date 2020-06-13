a719a1056693a30302eee4394fba0d01069b1e56
muddasir2929 <muddasir.mm74@gmail.com>
2020-06-13 21:01:31 +0500

    splice practice

--- chapter17-20/app.js
+++ chapter17-20/app.js
@@ -1,7 +1,12 @@
 var animalsName;
-animalsName = ["dog", "cat", "mouse", "rabbit"];
+animalsName = ["dog", "cat", "mouse", "rabbit", "cow"];
 // animalsName.shift();
 // animalsName.unshift("horse");
 // animalsName.pop();
-animalsName.push("lion", "fox")
+// animalsName.push("lion", "fox")
+// animalsName.splice(3, 2, "deer", "hen")
+// animalsName.splice(1, 3, "ass", "hen", "sheep")
+// animalsName.splice(2, 0, "pig", "duck")
+// animalsName.splice(2, 3)
 document.write(animalsName);
+
