e7312ffb7843799d0b335613866df7ada55dbf21
muddasir2929 <muddasir.mm74@gmail.com>
2020-06-15 00:30:10 +0500

    chapter 21-25

---
+++ chapter21-25/app.js
@@ -0,0 +1,14 @@
+// var city = prompt("enter your city name.");
+// var arr = ["lahore", "karachi", "peshawar"];
+// var city = city.toLowerCase();
+
+// for (var i=0; i<arr.length; i++){
+//     if (arr[i] === city){
+//         alert("match city found");
+//     }
+//     else{
+//         alert("match city not found");
+//     }
+//     break;
+// }
+
