91a7e229e9990b8b02e6c051c51e8961b58cf5ae
muddasir2929 <muddasir.mm74@gmail.com>
2020-06-17 18:37:05 +0500

    chapter 26-30 updated

---
+++ assignment # 6/chapter 26-30/app.js
@@ -0,0 +1,20 @@
+// question # 1
+
+// (a) number
+// var scoreAverage = 3.4521;
+// alert(scoreAverage);
+
+// (b) round method
+// var scoreAverage = 3.4521;
+// var numOfStars = Math.round(scoreAverage);
+// alert(numOfStars);
+
+// (c) floor method
+// var scoreAverage = 3.4521;
+// var numOfStars = Math.floor(scoreAverage);
+// alert(numOfStars);
+
+// (d) ceil method
+// var scoreAverage = 3.4521;
+// var numOfStars = Math.ceil(scoreAverage);
+// alert(numOfStars);
