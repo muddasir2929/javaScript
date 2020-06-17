8eb2027d1188628a32a939761c598bd8bb4d3516
muddasir2929 <muddasir.mm74@gmail.com>
2020-06-17 23:28:24 +0500

    chapter 26-30 updated

--- assignment # 6/chapter 26-30/app.js
+++ assignment # 6/chapter 26-30/app.js
@@ -18,3 +18,55 @@
 // var scoreAverage = 3.4521;
 // var numOfStars = Math.ceil(scoreAverage);
 // alert(numOfStars);
+
+
+
+
+// question # 2
+
+// (a) number
+// var scoreAverage = -2.673;
+// alert(scoreAverage);
+
+// (b) round method
+// var scoreAverage = -2.673;
+// var numOfStars = Math.round(scoreAverage);
+// alert(numOfStars);
+
+// (c) floor method
+// var scoreAverage = -2.673;
+// var numOfStars = Math.floor(scoreAverage);
+// alert(numOfStars);
+
+// (d) ceil method
+// var scoreAverage = -2.673;
+// var numOfStars = Math.ceil(scoreAverage);
+// alert(numOfStars);
+
+// question # 3
+// var x = -5;
+// var y = Math.abs(x);
+// alert(y);
+
+// question # 4
+// var diceNum = Math.floor(Math.random() * 6) + 1;
+// alert("random dice value " + diceNum);
+
+// question # 5
+// var userOne = prompt("enter heads username")
+// var userTwo = prompt("enter tail username")
+// var toss = Math.random() * 2;
+// var floor = Math.floor(toss);
+// if ( floor === 0){
+//     alert("Heads!" + " " + userOne + " " + "have won the toss!")
+// }
+// else {
+//     alert("Heads!" + " " + userTwo + " " + "have won the toss!")
+// }
+
+// question # 6
+// var bigDecimal = Math.random();
+// var floorDecimal = Math.floor((bigDecimal) * 100) + 1;
+// console.log(floorDecimal);\
+
+// question # 7
