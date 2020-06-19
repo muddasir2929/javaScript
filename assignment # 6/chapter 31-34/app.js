a5842d8499c8cd5792abc935abbc0bbfbc75b4ca
muddasir2929 <muddasir.mm74@gmail.com>
2020-06-19 11:36:57 +0500

    chapter 31-34

--- assignment # 6/chapter 31-34/app.js
+++ assignment # 6/chapter 31-34/app.js
@@ -5,3 +5,38 @@
 // alert(c);
 
 
+// qustion # 2
+// var monthArr = ["January", "February", "March", "April", "May", "June", "july", "August", "September", "October", "November", "December"];
+// var rightNow = new Date();
+// var theMonth = rightNow.getMonth();
+// var nameOfMonth = monthArr[theMonth];
+// alert("Current Month: " + nameOfMonth);
+
+
+// question # 3
+// var a = new Date();
+// var b = a.toString();
+// var c = b.slice(0, 3);
+// alert("Today is: " + c);
+
+// or
+
+// var dayNames = ["Sunday", "Monday", "Tueday", "Wednesday", "Thursday", "Friday", "Saturday"];
+// var now = new Date();
+// var theDay = now.getDay();
+// var nameOfToday = dayNames[theDay];
+// alert("Today is: " + nameOfToday);
+
+// question # 4
+// var dayNames = ["Sunday", "Monday", "Tueday", "Wednesday", "Thursday", "Friday", "Saturday"];
+// var now = new Date();
+// var theDay = now.getDay();
+// var nameOfToday = dayNames[theDay];
+// if (nameOfToday === "Sunday"){
+//     alert("It's fun today!");
+// }
+// else {
+//     alert("It's working day today!");
+// }
+
+// question # 5
