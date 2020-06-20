288058109e137a12e4157acd8550d63d7612e3fa
muddasir2929 <muddasir.mm74@gmail.com>
2020-06-20 15:27:55 +0500

    chapter 33-38

---
+++ assignment # 6/chapter 35-38/app.js
@@ -0,0 +1,62 @@
+// question # 1
+// function displayTime(){
+//     var timePkT = new Date();
+//     alert(timePkT);
+// }
+// displayTime();
+
+
+
+// function tellTime(){
+//     var now = new Date();
+//     var theHr = now.getHours();
+//     var theMin = now.getMinutes();
+//     alert("Current time: " + theHr + ":" + theMin);
+// }
+// tellTime();
+
+// question # 2
+
+
+
+// question # 3
+
+
+
+// question # 4
+
+
+
+// question # 5
+
+
+
+// question # 6
+
+
+
+// question # 7
+
+
+
+// question # 8
+
+
+
+// question # 9
+
+
+
+// question # 10
+
+
+
+// question # 11
+
+
+
+// question # 12
+
+// question # 13
+
+// question # 14
