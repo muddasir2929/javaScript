be7b0c65f594faed5b1c4b4d7c05ebb30a2ccc47
muddasir2929 <muddasir.mm74@gmail.com>
2020-06-22 18:40:58 +0500

    chapter 35-38

--- assignmet # 7/app.js
+++ assignmet # 7/app.js
@@ -4,6 +4,7 @@ switch (name) {
         alert("Hello hamid")
     case "basit":
         alert("Hello basit")
+        break;                    // this will break when it reached at basit. the word hello will not be appear in this code.
     default:
         alert("hello")
 }
