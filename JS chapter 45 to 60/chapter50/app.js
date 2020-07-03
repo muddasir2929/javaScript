06df6514f00a056b60b2648e24ec171d8f62d8ca
muddasir2929 <muddasir.mm74@gmail.com>
2020-07-04 00:39:43 +0500

    setting and reading paragraphs

---
+++ JS chapter 45 to 60/chapter50/app.js
@@ -0,0 +1,18 @@
+// function fillCity() {
+//     var cityName;
+//     var zipEntered = document.getElementById("zip").value;
+//     switch (zipEntered){
+//         case "60608":
+//             cityName = "Chicago";
+//             break;
+//         case "68114":
+//             cityName = "Omaha";
+//             break;
+//         case "66000":
+//             cityName = "Multan"
+//             break;
+//         case "53212":
+//             cityName = "Milwaukee";
+//     }
+//     document.getElementById("city").value = cityName;
+// }
---
+++ JS chapter 45 to 60/chapter51/app.js
@@ -0,0 +1,11 @@
+// function expandLoris() {
+//     var paraExpanded = 
+//     "this is Muhammad! and he is the student of business administration in Bahauddin Zakriya University  Multan, Pakistan";
+//     document.getElementById("slowLoris").innerHTML = paraExpanded;
+// }
+
+// function placeAList() {
+    // var listToPlace = 
+    // "<ol><li>slow loris</li><li>fast loris</li><li>just-right loris</li></ol>";
+    // document.getElementById("lorisList").innerHTML = listToPlace;
+// }
