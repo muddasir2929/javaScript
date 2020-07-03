b83c7d21fa2fbf172a84a9359bbc11e697131481
muddasir2929 <muddasir.mm74@gmail.com>
2020-07-03 20:17:11 +0500

    javaScript  reading field values

---
+++ JS chapter 45 to 60/chapter49/app.js
@@ -0,0 +1,14 @@
+// function checkAddress(fieldId){
+//     if (document.getElementById(fieldId).value === "") {
+//         alert("email address is required!");
+//     }
+// }
+
+// second case: some develpers prefer to put variables like this
+
+// function checkAddress(fieldId){
+//     var val = document.getElementById(fieldId).value;
+//     if (val === "") {
+//         alert("email address is required!");
+//     }
+// }
