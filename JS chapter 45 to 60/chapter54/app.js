2a6e7d435e65487ad327a350586fab21b157c70b
muddasir2929 <muddasir.mm74@gmail.com>
2020-07-04 07:44:51 +0500

    swapping images and setting text

---
+++ JS chapter 45 to 60/chapter54/app.js
@@ -0,0 +1,9 @@
+// function swapImg(){
+//     var pic = document.getElementById("before");
+//     pic.src = "../images/boysgreetings.jpg";
+// }
+
+// function swapImg(){
+//     var pic = document.getElementById("link1");
+//     var address = pic.href;
+// }
