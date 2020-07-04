9525df202c853d7496ddfeb90fde25fb5eefaa6a
muddasir2929 <muddasir.mm74@gmail.com>
2020-07-04 11:38:25 +0500

    hide content

---
+++ JS chapter 45 to 60/chapter55/app.js
@@ -0,0 +1,19 @@
+// function makeBig(){
+//     document.getElementById("p1").className += "big"
+// }
+
+// function makeBig() {
+//     document.getElementById("p1").style.fontSize = "2rem"
+// }
+
+// function floatImg() {
+//     document.getElementById("pic99").style.cssFloat = "right";
+// }
+
+// function hidElement() {
+//     document.getElementById("div9").style.visibility = "hidden";
+// }
+
+function centerImg(){
+    document.getElementById("pic99").style.margin = "0 auto;"
+}
