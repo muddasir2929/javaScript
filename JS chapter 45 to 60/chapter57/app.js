1294ae65de1e6a949412d44654ec315395ae9e11
muddasir2929 <muddasir.mm74@gmail.com>
2020-07-11 15:38:20 +0500

    targeted some elements in div

---
+++ JS chapter 45 to 60/chapter57/app.js
@@ -0,0 +1,6 @@
+// var a = document.getElementById("rules");
+// var paragrphs = a.getElementsByTagName("p");
+// for (var i = 0; i < paragrphs.length; i++){
+//     paragrphs[i].style.backgroundColor = "grey"
+//     paragrphs[i].style.color = "white"
+// }
