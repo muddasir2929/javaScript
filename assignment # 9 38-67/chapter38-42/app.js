8c62285abb7b4d0ffc0208637b1c78472528d399
muddasir2929 <muddasir.mm74@gmail.com>
2020-07-15 13:33:49 +0500

    js 38-67 chapter question 1

---
+++ assignment # 9 38-67/chapter38-42/app.js
@@ -0,0 +1,12 @@
+// var theSum = addNumbers();
+// function addNumbers(){
+//     var theSum = 3 + 3;
+//     return theSum
+// }
+// console.log(theSum);
+
+// question # 1
+// function myFunction(a, b){
+//     return Math.pow(2,4)
+// }
+// console.log(myFunction())
